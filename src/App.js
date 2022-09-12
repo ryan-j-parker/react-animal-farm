5dc17fbd82eda36d211881c82cd0a68833c74a96
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-09-12 15:11:26 -0700

    add background and render animal objects

--- src/App.js
+++ src/App.js
@@ -6,11 +6,17 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
 
+import Animal from './components/Animal/Animal.js';
+import { animals } from './data.js';
+
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      {animals.map((animal) => {
+        return <Animal key={animal.type} name={animal.name} type={animal.type} says={animal.says} />;
+      })}
       <Footer />
     </div>
   );
--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,9 @@
-export default function Animal() {
-  return <></>;
+export default function Animal(props) {
+  return (
+    <div className="animal" style={{ top: props.top, left: props.left }}>
+      <span className="name">{props.name}</span>
+      <span >{props.says}</span>
+      <img src={props.name.svg} /> 
+    </div>
+  ); 
 }
