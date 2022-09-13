536ccc057820d0f2c3334177b4a37aad83fa69cd
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-09-12 17:16:48 -0700

    change animal function to render the animal.says element only once, add audio files in advance of stretch goal

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -5,7 +5,6 @@ export default function Animal(props) {
     <div className="animal" style={{ top: props.top, left: props.left }}>
       <img src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
       <div className="name">{props.name}</div>
-      {props.type}
       {props.says}
     </div>
   ); 
